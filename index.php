1b49152232e4da6a3ebb6f6ca0a7a8fe78ab01b6
Marcos-Antonio1 <marcos_m10antonio@outlok.com>
2019-11-12 10:37:41 -0300

    primeiro Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Views/assets/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <title>home</title>
+</head>
+<body>
+     <div class="h1"> Estamos em construção </div>
+<script src="Views/assets/jquery-3.4.1.min.js"></script>
+<script src="Views/assets/bootstrap-4.3.1-dist/js/bootstrap.min.js"> </script>
+</body>
+</html>
