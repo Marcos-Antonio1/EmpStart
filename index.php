f965dc4f0f550f60de1850d589acca40b5043ec0
Marcos-Antonio1 <marcos_m10antonio@outlok.com>
2019-11-21 19:17:09 -0300

    atualizao no home do site

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="Views/assets/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="Views/assets/css/site.css">
-    <title>home</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="Views/assets/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="Views/assets/css/site.css">
+  <title>home</title>
 </head>
+
 <body>
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <a class="navbar-brand" href="#">
+        <img class="img-fluid" src="" alt=""> logo
+      </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -20,7 +24,6 @@
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
             <a class="nav-link" href="#">Home
-              <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
@@ -36,25 +39,45 @@
       </div>
     </div>
   </nav>
-
-  <!-- Page Content -->
-  <div class="container-fluid">
-
-    <!-- Heading Row -->
-    <div class="row align-items-center my-5">
-      <div class="col-lg-7">
-        <img class="img-fluid rounded mb-4 mb-lg-0" src="http://placehold.it/900x400" alt="">
+  <!--header -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-4  ">
+          <div class="container">
+            <div id="login" class="signin-card text-white">
+              <h1 class="display1">Entrar </h1>
+              <p class="subhead">Logue e veja o que há de novo </p>
+              <form action="" method="" class="" role="form" class="login">
+                <div id="form-login-username" class="form-group">
+                  <input id="username" class="form-control" name="username" type="text" size="18" alt="login" required />
+                  <span class="form-highlight"></span>
+                  <span class="form-bar"></span>
+                  <label for="username" class="float-label">login</label>
+                </div>
+                <div id="form-login-password" class="form-group">
+                  <input id="passwd" class="form-control" name="password" type="password" size="18" alt="password" required>
+                  <span class="form-highlight"></span>
+                  <span class="form-bar"></span>
+                  <label for="password" class="float-label">password</label>
+                </div>
+                <div>
+                  <button class="btn btn-block btn-info ripple-effect" type="submit" name="Submit" alt="sign in">Sign in</button>
+                </div>
+            </div>
+            </form>
+          </div>
+        </div>
+        <div class="col-12 col-md-8 texto">
+          <h1>informações</h1>
+          <h4 class="h4"> besteiras escrita</h2>
+        </div>
       </div>
-      <!-- /.col-lg-8 -->
-      <div class="col-lg-5">
-        <h1 class="font-weight-light">Business Name or Tagline</h1>
-        <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
-        <a class="btn btn-primary" href="#">Call to Action!</a>
-      </div>
-      <!-- /.col-md-4 -->
     </div>
-    <!-- /.row -->
-
+    </div>
+  </header>
+  <div class="container-fluid">
     <!-- Call to Action Well -->
     <div class="card text-white bg-secondary my-5 py-4 text-center">
       <div class="card-body">
@@ -115,7 +138,8 @@
     <!-- /.container -->
   </footer>
 
-<script src="Views/assets/jquery-3.4.1.min.js"></script>
-<script src="Views/assets/bootstrap-4.3.1-dist/js/bootstrap.min.js"> </script>
+  <script src="Views/assets/jquery-3.4.1.min.js"></script>
+  <script src="Views/assets/bootstrap-4.3.1-dist/js/bootstrap.min.js"> </script>
 </body>
+
 </html>
