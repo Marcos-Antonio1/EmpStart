b4e976e6381b28176b70d0a31ed7b3a082cca2e0
Arthur Alaete <arthuralaetelopes@gmail.com>
2019-11-22 23:14:24 -0300

    Básico dos Cadastros de usuários e projetos(interface)

--- Views/cadastro.php
+++ Views/cadastro.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="assets/bootstrap-4.3.1-dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="Views/assets/css/site.css">
-  <title>home</title>
-</head>
-<div class="card" style="margin: 40px">
+<?php include "template/header.php"?>
+
+<div class="card" style="margin: 40px" style="margin-top: 80px">
   <form class="container" style="margin: 30px;" method="POST" action="#">
     <div class="form-row">
         <div class="form-group col-md-6">
@@ -64,7 +56,5 @@
     <br><br><button type="submit" class="btn btn-info">Cadastrar</button>
   </form>
 </div>
-<script src="Views/assets/jquery-3.4.1.min.js"></script>
-<script src="Views/assets/bootstrap-4.3.1-dist/js/bootstrap.min.js"> </script>
 </body>
-</html>
+<?php include "template/footer.php"?>
---
+++ Views/cadastroProjeto.php
@@ -0,0 +1,39 @@
+<?php include "template/header.php"?>
+
+<div class="card" style="margin: 40px" style="margin-top: 80px">
+  <form class="container" style="margin: 30px;" method="POST" action="#">
+    <div class="form-row">
+        <div class="form-group col-md-6">
+            <label for="nome">Nome do Projeto</label>
+            <input type="text" class="form-control" id="nome" placeholder="Digite o nome do projeto" required="required">
+          </div>
+      <div class="form-group col-md-4">
+        <label for="area">Área do Projeto</label>
+        <select id="area" class="form-control" required="required">
+          <option selected>Selecione uma área</option>
+          <option>...</option>
+        </select>
+      </div>
+      <div class="form-group col-md-9">
+        <label for="descricao">Descrição do Projeto</label>
+        <textarea class="form-control" id="descricao" rows="5" placeholder="Faça uma descrição sobre seu projeto"></textarea>
+    </div>
+    </div>
+    <h6 for="">Inicar projeto com disponibilidade para investimentos: </h6>
+    <div class="form-check-inline">
+        <input class="form-check-input" type="radio" name="tipo" id="Empreendedor" value="opcao1" checked>
+        <label class="form-check-label" for="Empreendedor">
+          Disponível
+        </label>
+      </div>
+      <div class="form-check-inline">
+        <input class="form-check-input" type="radio" name="tipo" id="Investidor" value="opcao2">
+        <label class="form-check-label" for="Investidor">
+          Não disponível
+        </label>
+    </div>
+    <br><br><button type="submit" class="btn btn-info">Cadastrar Projeto</button>
+  </form>
+</div>
+</body>
+<?php include "template/footer.php"?>
